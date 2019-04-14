25a370a3ed0b3e3a6f4d12e1b132cc0be346c2c8
Blank <ellenmacy@Lindsays-MacBook-Pro.local>
2019-04-14 15:14:14 -0400

    got spotify function to work

--- liri.js
+++ liri.js
@@ -1,36 +1,95 @@
 require("dotenv").config();
 
+let moment = require('moment');
+
 let keys = require("./keys.js");
 
-let spotify = new Spotify(keys.spotify);
+//let spotify = new Spotify(keys.spotify);
 
-/* let spotify = require('node-spotify-api');
-let axios = require("axios");
-let moment = require("moment");
-let dotenv = require("dotenv");
- */
+let Spotify = require('node-spotify-api');
+//let dotenv = require("dotenv");
+
 
 let axios = require("axios");
 
 let command = process.argv[2];
 let input = process.argv.slice(3).join("+");
 
+if (command === "concert-this") {
 
+    axios.get(`https://rest.bandsintown.com/artists/${input}/?app_id=codingbootcamp`).then(
+        function (response) {
+
+
+            console.log("The artist/band's name is: " + response.data.name);
+
+        }
+    );
+
+    // We then run the request with axios module on a URL with a JSON
+    axios.get(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`).then(
+        function (response) {
+            //console.log(response);
+
+            /*  let venue = response.venue.name;
+             let date = moment(response.datetime).format("MM/DD/YYYY");
+             let location = `${response.venue.city}, ${response.venue.country}`; */
+
+            //console.log("The artist/band's name is: " + input);
+            console.log("The artist/band's is playing at: " + response.data[0].venue.name);
+            console.log("Location of the Venue: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+            console.log("Date of the event: " + response.data[0].venue.city);
+
+        }
+    );
+
+};
+
+let spotify = new Spotify({
+    id: 'ebb9fd21b387499b962ede43a9212592',
+    secret: 'a556a21aba2348e4ab35a3bb38bb7b0d'
+});
+
+
+if (command === "spotify-this-song") {
+
+    spotify.search({ type: 'track', query: input }).then(
+        function (response) {
+            //console.log(response);
+            
+           /*  console.log(response.tracks);
+            console.log("----------------------------------------"); */
+            /* console.log(response.tracks.items);
+            console.log("----------------------------------------"); */
+            console.log(response.tracks.items[0]); 
+            console.log("----------------------------------------");
+            console.log("this is the song name " + response.tracks.items[0].name);
+            console.log("----------------------------------------");
+            console.log("this is the link to the track preview " + response.tracks.items[0].album.preview_url);
+           
+
+
+            /* console.log(response.tracks.items[0].album);
+            console.log("----------------------------------------");
+            console.log(response.tracks.items[0].album.artists);
+            console.log("----------------------------------------"); */
+            console.log("this gets the artist/band's name " + response.tracks.items[0].album.artists[0].name);
+            console.log("----------------------------------------");
+            console.log("this gets the album name " + response.tracks.items[0].album.name);
+            console.log("----------------------------------------");
+            /* console.log(response.tracks.items[0].album.external_urls);
+            console.log("----------------------------------------"); */
+            console.log("this gets the link to the album " + response.tracks.items[0].album.external_urls.spotify);
+    });
+    //CREATE IF ELSE TO GET "The Sign" by Ace of Base WHEN NOTHING COMES UP
+};
 
 if (command === "movie-this") {
 
     // We then run the request with axios module on a URL with a JSON
     axios.get(`http://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=trilogy`).then(
         function (response) {
-            // Then we print out the imdbRating
-            /*  * Title of the movie.
-             * Year the movie came out.
-             * IMDB Rating of the movie.
-             * Rotten Tomatoes Rating of the movie.
-             * Country where the movie was produced.
-             * Language of the movie.
-             * Plot of the movie.
-             * Actors in the movie. */
+
             console.log("The movie's title is: " + response.data.Title);
             console.log("The movie's release year is: " + response.data.Year);
             console.log("The movie's IMD rating is: " + response.data.imdbRating);
@@ -42,5 +101,24 @@ if (command === "movie-this") {
         }
     );
 
-}
+};
 
+
+if (command === "do-what-it-says") {
+
+    // We then run the request with axios module on a URL with a JSON
+    axios.get(`http://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=trilogy`).then(
+        function (response) {
+
+            console.log("The movie's title is: " + response.data.Title);
+            console.log("The movie's release year is: " + response.data.Year);
+            console.log("The movie's IMD rating is: " + response.data.imdbRating);
+            console.log("The movie's Rotten Totmatoes Rating is: " + response.data.Ratings[1].Value);
+            console.log("The movie's country of production: " + response.data.Country);
+            console.log("The movie's language is in: " + response.data.Language);
+            console.log("The movie's plot is: " + response.data.Plot);
+            console.log("The movie's actors are: " + response.data.Actors);
+        }
+    );
+
+};
