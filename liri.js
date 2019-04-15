b733a2bc998fc829de8072321de57233a3513cf3
Blank <ellenmacy@Lindsays-MacBook-Pro.local>
2019-04-15 19:51:16 -0400

    get do what it says working

--- liri.js
+++ liri.js
@@ -4,12 +4,6 @@ let moment = require('moment');
 
 let keys = require("./keys.js");
 
-//let spotify = new Spotify(keys.spotify);
-
-let Spotify = require('node-spotify-api');
-//let dotenv = require("dotenv");
-
-
 let axios = require("axios");
 
 let command = process.argv[2];
@@ -45,6 +39,8 @@ if (command === "concert-this") {
 
 };
 
+let Spotify = require('node-spotify-api');
+
 let spotify = new Spotify({
     id: 'ebb9fd21b387499b962ede43a9212592',
     secret: 'a556a21aba2348e4ab35a3bb38bb7b0d'
@@ -56,17 +52,17 @@ if (command === "spotify-this-song") {
     spotify.search({ type: 'track', query: input }).then(
         function (response) {
             //console.log(response);
-            
-           /*  console.log(response.tracks);
-            console.log("----------------------------------------"); */
+
+            /*  console.log(response.tracks);
+             console.log("----------------------------------------"); */
             /* console.log(response.tracks.items);
             console.log("----------------------------------------"); */
-            console.log(response.tracks.items[0]); 
+            console.log(response.tracks.items[0]);
             console.log("----------------------------------------");
             console.log("this is the song name " + response.tracks.items[0].name);
             console.log("----------------------------------------");
             console.log("this is the link to the track preview " + response.tracks.items[0].album.preview_url);
-           
+
 
 
             /* console.log(response.tracks.items[0].album);
@@ -80,7 +76,9 @@ if (command === "spotify-this-song") {
             /* console.log(response.tracks.items[0].album.external_urls);
             console.log("----------------------------------------"); */
             console.log("this gets the link to the album " + response.tracks.items[0].album.external_urls.spotify);
-    });
+        });
+
+
     //CREATE IF ELSE TO GET "The Sign" by Ace of Base WHEN NOTHING COMES UP
 };
 
@@ -103,22 +101,35 @@ if (command === "movie-this") {
 
 };
 
+let fs = require("fs");
 
 if (command === "do-what-it-says") {
 
-    // We then run the request with axios module on a URL with a JSON
-    axios.get(`http://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=trilogy`).then(
-        function (response) {
-
-            console.log("The movie's title is: " + response.data.Title);
-            console.log("The movie's release year is: " + response.data.Year);
-            console.log("The movie's IMD rating is: " + response.data.imdbRating);
-            console.log("The movie's Rotten Totmatoes Rating is: " + response.data.Ratings[1].Value);
-            console.log("The movie's country of production: " + response.data.Country);
-            console.log("The movie's language is in: " + response.data.Language);
-            console.log("The movie's plot is: " + response.data.Plot);
-            console.log("The movie's actors are: " + response.data.Actors);
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
         }
-    );
+
+        //console.log(data);
+
+        input = data.split(" ").join("+");
+
+        spotify.search({ type: 'track', query: input }).then(
+            function (response) {
+               
+                console.log("Song name: " + response.tracks.items[0].name);
+                console.log("----------------------------------------");
+                console.log("Link to the track preview: " + response.tracks.items[0].album.preview_url);
+                console.log("----------------------------------------");
+                console.log("The artist/band's name: " + response.tracks.items[0].album.artists[0].name);
+                console.log("----------------------------------------");
+                console.log("The album name: " + response.tracks.items[0].album.name);
+                console.log("----------------------------------------");
+                console.log("The link to the album: " + response.tracks.items[0].album.external_urls.spotify);
+            });
+    
+
+
+    });
 
 };
