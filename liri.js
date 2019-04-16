95f07551881fb3b96bf7877e8447fc59204ce9e3
Blank <ellenmacy@Lindsays-MacBook-Pro.local>
2019-04-16 18:20:13 -0400

    fixing issues with spotify

--- liri.js
+++ liri.js
@@ -1,15 +1,53 @@
 require("dotenv").config();
 
-let moment = require('moment');
+const moment = require('moment');
 
-let keys = require("./keys.js");
+const keys = require("./keys.js");
 
-let axios = require("axios");
+const axios = require("axios");
+
+const fs = require('fs');
+
+const Spotify = require('node-spotify-api');
+
+const spotify = new Spotify({
+    id: 'ebb9fd21b387499b962ede43a9212592',
+    secret: 'a556a21aba2348e4ab35a3bb38bb7b0d'
+});
+
 
 let command = process.argv[2];
 let input = process.argv.slice(3).join("+");
 
-if (command === "concert-this") {
+switch (command) {
+    case "concert-this":
+        concert();
+        break;
+
+    case "spotify-this-song":
+        runSpotify();
+        break;
+
+    case "movie-this":
+        movie();
+        break;
+
+    case "do-what-it-says":
+        other();
+        break;
+}
+
+function concert() {
+
+    axios.get(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`).then(
+        function (response) {
+
+            console.log(`Next ${input} concert is at: ${response.data[0].venue.name}`);
+            console.log("Location of the Venue: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+            console.log("Date of the event: " + response.data[0].venue.city);
+
+        }
+    );
 
     axios.get(`https://rest.bandsintown.com/artists/${input}/?app_id=codingbootcamp`).then(
         function (response) {
@@ -19,111 +57,85 @@ if (command === "concert-this") {
         }
     );
 
-    // We then run the request with axios module on a URL with a JSON
-    axios.get(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`).then(
-        function (response) {
-            
-            console.log("The artist/band's is playing at: " + response.data[0].venue.name);
-            console.log("Location of the Venue: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
-            console.log("Date of the event: " + response.data[0].venue.city);
-
-        }
-    );
-
 };
 
-let Spotify = require('node-spotify-api');
-
-let spotify = new Spotify({
-    id: 'ebb9fd21b387499b962ede43a9212592',
-    secret: 'a556a21aba2348e4ab35a3bb38bb7b0d'
-});
-
-
-if (command === "spotify-this-song") {
-    
+function spotifyInfo() {
     spotify.search({ type: 'track', query: input }).then(
         function (response) {
-
-
-            //console.log(response);
-
-            /*  console.log(response.tracks);
-             console.log("----------------------------------------"); */
-            /* console.log(response.tracks.items);
-            console.log("----------------------------------------"); */
-            console.log(response.tracks.items[0]);
-            console.log("----------------------------------------");
-            console.log("this is the song name " + response.tracks.items[0].name);
+            console.log("Song name: " + response.tracks.items[0].name);
             console.log("----------------------------------------");
             console.log("this is the link to the track preview " + response.tracks.items[0].album.preview_url);
-
-
-
-            /* console.log(response.tracks.items[0].album);
             console.log("----------------------------------------");
-            console.log(response.tracks.items[0].album.artists);
-            console.log("----------------------------------------"); */
             console.log("this gets the artist/band's name " + response.tracks.items[0].album.artists[0].name);
             console.log("----------------------------------------");
             console.log("this gets the album name " + response.tracks.items[0].album.name);
             console.log("----------------------------------------");
-            /* console.log(response.tracks.items[0].album.external_urls);
-            console.log("----------------------------------------"); */
             console.log("this gets the link to the album " + response.tracks.items[0].album.external_urls.spotify);
-        });
-
-
-    //CREATE IF ELSE TO GET "The Sign" by Ace of Base WHEN NOTHING COMES UP
-};
-
-if (command === "movie-this") {
-
-    // We then run the request with axios module on a URL with a JSON
-    axios.get(`http://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=trilogy`).then(
-        function (response) {
-
-            console.log("The movie's title is: " + response.data.Title);
-            console.log("The movie's release year is: " + response.data.Year);
-            console.log("The movie's IMD rating is: " + response.data.imdbRating);
-            console.log("The movie's Rotten Totmatoes Rating is: " + response.data.Ratings[1].Value);
-            console.log("The movie's country of production: " + response.data.Country);
-            console.log("The movie's language is in: " + response.data.Language);
-            console.log("The movie's plot is: " + response.data.Plot);
-            console.log("The movie's actors are: " + response.data.Actors);
-        }
+        } 
     );
+}
+
+
+function runSpotify() {
+
+    input = process.argv.slice(3).join("+");
+
+    if (!input) {
+     
+        input = "The+Sign"
+        
+        spotifyInfo();
+
+    } else {
+        
+        spotifyInfo();
+
+    };
 
 };
 
-let fs = require("fs");
+function movieInfo() {
+    axios.get(`http://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=trilogy`).then(
+            function (response) {
 
-if (command === "do-what-it-says") {
+                console.log("The movie's title is: " + response.data.Title);
+                console.log("The movie's release year is: " + response.data.Year);
+                console.log("The movie's IMD rating is: " + response.data.imdbRating);
+                console.log("The movie's Rotten Totmatoes Rating is: " + response.data.Ratings[1].Value);
+                console.log("The movie's country of production: " + response.data.Country);
+                console.log("The movie's language is in: " + response.data.Language);
+                console.log("The movie's plot is: " + response.data.Plot);
+                console.log("The movie's actors are: " + response.data.Actors);
+            }
+        );
+}
+
+function movie() {
+
+    if (!input) {
+        input = 'mr.nobody'
+        movieInfo();
+    }
+
+    else {
+        movieInfo();
+    }
+
+};
+
+function other() {
 
     fs.readFile("random.txt", "utf8", function (error, data) {
+
         if (error) {
             return console.log(error);
         }
 
-        //console.log(data);
+        console.log(data);
 
         input = data.split(" ").join("+");
 
-        spotify.search({ type: 'track', query: input }).then(
-            function (response) {
-               
-                console.log("Song name: " + response.tracks.items[0].name);
-                console.log("----------------------------------------");
-                console.log("Link to the track preview: " + response.tracks.items[0].album.preview_url);
-                console.log("----------------------------------------");
-                console.log("The artist/band's name: " + response.tracks.items[0].album.artists[0].name);
-                console.log("----------------------------------------");
-                console.log("The album name: " + response.tracks.items[0].album.name);
-                console.log("----------------------------------------");
-                console.log("The link to the album: " + response.tracks.items[0].album.external_urls.spotify);
-            });
-    
-
+        spotifyInfo();
 
     });
 
